9efd34cd7850b30649f0ca682ee116f9a8207705
nicholash85 <71468132+nicholash85@users.noreply.github.com>
2022-03-07 12:40:31 -0500

    Create install_script.sh

---
+++ install_script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Get script directory
+BASE_DIR=$(realpath $(dirname "$0"))
+
+# Pull down installer script and run it
+echo "Do you want to download and install conda?"
+select ans in "Yes" "No"; do
+  case $ans in
+    Yes)
+      wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $BASE_DIR/conda_tmp.sh
+      chmod u+x $BASE_DIR/conda_tmp.sh
+      $BASE_DIR/conda_tmp.sh -u
+      rm $BASE_DIR/conda_tmp.sh
+      break
+      ;;
+    No)
+      break
+      ;;
+  esac
+done
+
+# Create symlink
+ln -sf $BASE_DIR/condarc $HOME/.condarc
+
+exit 0
