acb0a0b9d05311df1ac96d3c099fcd807268bf4f
Nicholas Haehn <haehnnicholas@gmail.com>
2022-03-10 21:31:53 -0500

    Added Training

--- install_script.sh
+++ install_script.sh
@@ -3,22 +3,11 @@
 # Get script directory
 BASE_DIR=$(realpath $(dirname "$0"))
 
-# Pull down installer script and run it
-echo "Do you want to download and install conda?"
-select ans in "Yes" "No"; do
-  case $ans in
-    Yes)
-      wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $BASE_DIR/conda_tmp.sh
-      chmod u+x $BASE_DIR/conda_tmp.sh
-      $BASE_DIR/conda_tmp.sh -u
-      rm $BASE_DIR/conda_tmp.sh
-      break
-      ;;
-    No)
-      break
-      ;;
-  esac
-done
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $BASE_DIR/conda_tmp.sh
+chmod u+x $BASE_DIR/conda_tmp.sh
+$BASE_DIR/conda_tmp.sh -u
+rm $BASE_DIR/conda_tmp.sh
 
 # Create symlink
 ln -sf $BASE_DIR/condarc $HOME/.condarc
