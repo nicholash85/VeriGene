91783e7eec095577a067e7edb7c607cdaa08ff85
nicholash85 <71468132+nicholash85@users.noreply.github.com>
2022-03-31 19:27:01 -0400

    Update ResplitData.py

--- ResplitData.py
+++ ResplitData.py
@@ -1,6 +1,7 @@
 import shutil
 import os
 import random
+import sys
 
 from pip import main
 
@@ -25,16 +26,45 @@ def mkdirsInfUninf(Foldername):
   mkdir(Foldername+'/Uninfected')
   mkdir(Foldername+'/Infected')
 
+def emptyDirectories(FilePath):
+  print("Emptying Directories")
+  for test in ["Test","Train","Validation"]:
+  # Empty Directories
+    folder = os.path.join(sys.path[0], FilePath + '/' + test + '/Uninfected')
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+    folder = os.path.join(sys.path[0], FilePath + '/' + test + '/Infected')
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+  print(FilePath+" emptied.")
+
 def ResplitData(InputFolder,OutputFolder,TrainSplit,ValSplit,TestSplit):
   mkSplitDirs(OutputFolder)
+  emptyDirectories(OutputFolder)
 
   if TrainSplit+ValSplit+TestSplit != 100:
     raise
 
   for name in [InputFolder+'/Test/',InputFolder+'/Train/',InputFolder+'/Validation/']:
     for name2 in ['Uninfected/','Infected/']:
-      for nameVerilog in os.listdir(name+name2):
-        nameVerilog = os.path.join(name+name2,nameVerilog)
+      for nameVerilog1 in os.listdir(name+name2):
+        nameVerilog = os.path.join(name+name2,nameVerilog1)
 
         rand = random.randint(0, 100)
         if rand < TrainSplit:
@@ -44,7 +74,10 @@ def ResplitData(InputFolder,OutputFolder,TrainSplit,ValSplit,TestSplit):
         else:
           test = 'Test'
         
-        shutil.copy(nameVerilog,OutputFolder+'/'+test+'/'+name2)
+        outputLocation = OutputFolder+'/'+test+'/'+name2
+        shutil.copy(nameVerilog,outputLocation)
+
+        print("Moved "+nameVerilog+ " to "+ outputLocation+nameVerilog1+"\n")
 
 
 ##########
