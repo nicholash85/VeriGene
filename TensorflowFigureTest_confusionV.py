6c5f3ea36671200e8076dd46bea07e34eba60876
nicholash85 <71468132+nicholash85@users.noreply.github.com>
2022-03-31 15:16:23 -0400

    Update TensorflowFigureTest_confusionV.py

--- TensorflowFigureTest_confusionV.py
+++ TensorflowFigureTest_confusionV.py
@@ -189,6 +189,7 @@ history = model.fit(train_ds, validation_data=val_ds, epochs=epochs,callbacks=[c
 #Split Test_ds into x and y
 predictions = model.predict(test_ds)
 prediction_classes = [1 if prob > 0.5 else 0 for prob in np.ravel(predictions)]
+print(prediction_classes)
 print(confusion_matrix(y, prediction_classes))
 
 print(model.summary())
