ca3913e992eecb511ec117a626e08ab4bd4199ce
nicholash85 <71468132+nicholash85@users.noreply.github.com>
2022-03-31 20:28:28 -0400

    Update TensorflowFigureTest_confusionV.py

--- TensorflowFigureTest_confusionV.py
+++ TensorflowFigureTest_confusionV.py
@@ -92,8 +92,8 @@ print("Vectorized DNA: ", vectorize_text(first_DNA, first_label))
 # print("1 ---> ",vectorize_layer.get_vocabulary()[1])
 # print("4 ---> ",vectorize_layer.get_vocabulary()[4])
 print('Vocabulary size: {}'.format(len(vectorize_layer.get_vocabulary())))
-for k in range(0,len(vectorize_layer.get_vocabulary())):
-  print("{0} ---> {1}".format(k, vectorize_layer.get_vocabulary()[k]))
+# for k in range(0,len(vectorize_layer.get_vocabulary())):
+#   print("{0} ---> {1}".format(k, vectorize_layer.get_vocabulary()[k]))
 
 train_ds = raw_train_ds.map(vectorize_text)
 val_ds = raw_val_ds.map(vectorize_text)
@@ -190,6 +190,7 @@ history = model.fit(train_ds, validation_data=val_ds, epochs=epochs,callbacks=[c
 print(len(test_ds))
 predictions = model.predict(test_ds)
 # print(len(predictions))
+# print(predictions)
 prediction_classes = [1 if prob > 0.5 else 0 for prob in np.ravel(predictions)]
 # print(y)
 # print(prediction_classes)
