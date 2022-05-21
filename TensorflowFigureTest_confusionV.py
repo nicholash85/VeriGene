26056e4847ecd396a7daed30488c80197564d2cc
nicholash85 <71468132+nicholash85@users.noreply.github.com>
2022-05-21 18:54:19 -0400

    Update TensorflowFigureTest_confusionV.py

--- TensorflowFigureTest_confusionV.py
+++ TensorflowFigureTest_confusionV.py
@@ -202,7 +202,7 @@ for qw in range(0,len(np.ravel(predictions))):
     print("np.ravel(predictions): " + str(qw) + ": " + str(np.ravel(predictions)[qw]))
 prediction_classes = [1 if prob > 0.5 else 0 for prob in np.ravel(predictions)]
 # print(y)
-# print(prediction_classes)
+print(prediction_classes)
 print("len(x):" + str(len(x)))
 print("len(y):" + str(len(y)))
 for qw in range(0,len(x)):
